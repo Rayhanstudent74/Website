d15584b708f9558bd5229fc70cbb7dd20d586deb
rizve_ahamed <rayhan3487@gmail.com>
2024-02-02 11:33:21 +0600

    guessgame

---
+++ index.js
@@ -0,0 +1,8 @@
+//alert("hello");
+document.write("Rizve Ahamed Raihan<br>");
+document.write("01212342312441<br>");
+document.write("Software Engineer<br>");
+ var nam = "hello";
+// document.write(nam);
+nam="salam";
+document.write(nam);
